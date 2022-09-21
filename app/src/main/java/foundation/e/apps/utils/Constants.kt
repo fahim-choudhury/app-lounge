8d0e65cfd5dd0b2a9d0e7d6317fdabf5da3190d1
Sayantan Roychowdhury <sayantan.rc@e.email>
2022-09-21 12:42:58 +0000

    MR 3 - Login API classes and repositories

--- app/src/main/java/foundation/e/apps/utils/Constants.kt
+++ app/src/main/java/foundation/e/apps/utils/Constants.kt
@@ -2,4 +2,8 @@ package foundation.e.apps.utils
 
 object Constants {
     const val timeoutDurationInMillis: Long = 25000
+
+    const val PREFERENCE_SHOW_FOSS = "showFOSSApplications"
+    const val PREFERENCE_SHOW_PWA = "showPWAApplications"
+    const val PREFERENCE_SHOW_GPLAY = "showAllApplications"
 }
