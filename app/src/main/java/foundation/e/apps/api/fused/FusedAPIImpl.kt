9dcdf248ed42df250883f1c19185446afe64331f
Hasib Prince <hasibprince@e.email>
2022-11-02 10:50:37 +0000

    Issue 742, 6064: fixed: showing gplay search result

--- app/src/main/java/foundation/e/apps/api/fused/FusedAPIImpl.kt
+++ app/src/main/java/foundation/e/apps/api/fused/FusedAPIImpl.kt
@@ -244,7 +244,16 @@ class FusedAPIImpl @Inject constructor(
                     query,
                     searchResult,
                     packageSpecificResults
-                )?.let { emit(it) }
+                ).let { emit(it) }
+            }
+
+            if (preferenceManagerModule.isPWASelected()) {
+                fetchPWASearchResult(
+                    this@FusedAPIImpl,
+                    query,
+                    searchResult,
+                    packageSpecificResults
+                ).let { emit(it) }
             }
 
             if (preferenceManagerModule.isGplaySelected()) {
@@ -257,15 +266,6 @@ class FusedAPIImpl @Inject constructor(
                     )
                 )
             }
-
-            if (preferenceManagerModule.isPWASelected()) {
-                fetchPWASearchResult(
-                    this@FusedAPIImpl,
-                    query,
-                    searchResult,
-                    packageSpecificResults
-                )?.let { emit(it) }
-            }
         }
     }
 
@@ -274,7 +274,7 @@ class FusedAPIImpl @Inject constructor(
         query: String,
         searchResult: MutableList<FusedApp>,
         packageSpecificResults: ArrayList<FusedApp>
-    ): ResultSupreme<Pair<List<FusedApp>, Boolean>>? {
+    ): ResultSupreme<Pair<List<FusedApp>, Boolean>> {
         val pwaApps: MutableList<FusedApp> = mutableListOf()
         val status = fusedAPIImpl.runCodeBlockWithTimeout({
             getCleanAPKSearchResults(
@@ -290,19 +290,19 @@ class FusedAPIImpl @Inject constructor(
 
         if (pwaApps.isNotEmpty() || status != ResultStatus.OK) {
             searchResult.addAll(pwaApps)
-            return ResultSupreme.create(
-                status,
-                Pair(
-                    filterWithKeywordSearch(
-                        searchResult,
-                        packageSpecificResults,
-                        query
-                    ),
-                    false
-                )
-            )
         }
-        return null
+
+        return ResultSupreme.create(
+            status,
+            Pair(
+                filterWithKeywordSearch(
+                    searchResult,
+                    packageSpecificResults,
+                    query
+                ),
+                preferenceManagerModule.isGplaySelected()
+            )
+        )
     }
 
     private fun fetchGplaySearchResults(
@@ -333,26 +333,26 @@ class FusedAPIImpl @Inject constructor(
         query: String,
         searchResult: MutableList<FusedApp>,
         packageSpecificResults: ArrayList<FusedApp>
-    ): ResultSupreme<Pair<List<FusedApp>, Boolean>>? {
+    ): ResultSupreme<Pair<List<FusedApp>, Boolean>> {
         val status = fusedAPIImpl.runCodeBlockWithTimeout({
             cleanApkResults.addAll(getCleanAPKSearchResults(query))
         })
 
-        if (cleanApkResults.isNotEmpty() || status != ResultStatus.OK) {
+        if (cleanApkResults.isNotEmpty()) {
             searchResult.addAll(cleanApkResults)
-            return ResultSupreme.create(
-                status,
-                Pair(
-                    filterWithKeywordSearch(
-                        searchResult,
-                        packageSpecificResults,
-                        query
-                    ),
-                    preferenceManagerModule.isGplaySelected() || preferenceManagerModule.isPWASelected()
-                )
-            )
         }
-        return null
+
+        return ResultSupreme.create(
+            status,
+            Pair(
+                filterWithKeywordSearch(
+                    searchResult,
+                    packageSpecificResults,
+                    query
+                ),
+                preferenceManagerModule.isGplaySelected() || preferenceManagerModule.isPWASelected()
+            )
+        )
     }
 
     private suspend fun fetchPackageSpecificResult(
