3ab57cf69e7f04e472f77d73cc22521366d9f980
Sayantan Roychowdhury <sayantan.rc@e.email>
2023-01-30 16:45:20 +0530

    Revert "Issue 854: Integrate sentry"
    This reverts commit 53ca2d02

--- app/src/main/java/foundation/e/apps/AppLoungeApplication.kt
+++ app/src/main/java/foundation/e/apps/AppLoungeApplication.kt
@@ -28,7 +28,6 @@ import foundation.e.apps.manager.pkg.PkgManagerModule
 import foundation.e.apps.manager.workmanager.InstallWorkManager
 import foundation.e.apps.setup.tos.TOS_VERSION
 import foundation.e.apps.utils.modules.DataStoreModule
-import foundation.e.lib.telemetry.Telemetry
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
@@ -64,19 +63,14 @@ class AppLoungeApplication : Application(), Configuration.Provider {
                 dataStoreModule.saveTOCStatus(false, "")
             }
         }
-
         if (BuildConfig.DEBUG) {
             plant(Timber.DebugTree())
         } else {
-            // Allow enabling telemetry only for release builds.
-            Telemetry.init(BuildConfig.SENTRY_DSN, this)
             plant(object : Timber.Tree() {
                 override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                    Telemetry.reportMessage("$tag: $message")
                     if (priority == Log.DEBUG || priority == Log.VERBOSE) {
                         return
                     }
-
                     Log.println(priority, tag, message)
                 }
             })
