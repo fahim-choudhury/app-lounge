9796ce3c64b9381162e543af5de2fe0d482fb878
hasibprince <hasibprince@e.email>
2023-03-09 19:41:13 +0600

    some refactoring

--- app/src/main/java/foundation/e/apps/api/StoreApiRepository.kt
+++
@@ -1,7 +0,0 @@
-package foundation.e.apps.api
-
-interface StoreApiRepository {
-    suspend fun getHomeScreenData(): Any
-    suspend fun getSearchResult(query: String): Any
-    suspend fun getSearchSuggestions(query: String): Any
-}
--- app/src/main/java/foundation/e/apps/api/cleanapk/CleanApkAppsRepository.kt
+++ app/src/main/java/foundation/e/apps/api/cleanapk/CleanApkAppsRepository.kt
@@ -1,14 +1,32 @@
+/*
+ * Copyright MURENA SAS 2023
+ * Apps  Quickly and easily install Android apps onto your device!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package foundation.e.apps.api.cleanapk
 
-import foundation.e.apps.api.StoreApiRepository
+import foundation.e.apps.api.StoreRepository
 import foundation.e.apps.api.cleanapk.data.home.HomeScreen
 import foundation.e.apps.api.cleanapk.data.search.Search
 import retrofit2.Response
 
 class CleanApkAppsRepository(
     private val cleanAPKInterface: CleanAPKInterface,
-    private val cleanApkAppDetailApi: CleanApkAppDetailApi
-) : StoreApiRepository {
+) : StoreRepository {
+
     override suspend fun getHomeScreenData(): Response<HomeScreen> {
         return cleanAPKInterface.getHomeScreenData(
             CleanAPKInterface.APP_TYPE_ANY,
