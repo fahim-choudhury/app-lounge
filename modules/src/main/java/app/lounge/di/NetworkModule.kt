dc40dc7f09e4acb316f53ea358acf02649f0a468
Nishant Dande <nsdande@murena.io>
2023-07-16 21:16:39 +0530

    Implement network api using coroutine

---
+++ modules/src/main/java/app/lounge/di/NetworkModule.kt
@@ -0,0 +1,59 @@
+package app.lounge.di
+
+import app.lounge.networking.NetworkFetchingRetrofitAPI
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    private const val HTTP_TIMEOUT = 10L
+
+    @Provides
+    @Singleton
+    internal fun providesRetrofit(
+        okHttpClient: OkHttpClient,
+        baseUrl: String = NetworkFetchingRetrofitAPI.tokenBaseURL
+    ) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("privateOkHttpClient")
+    internal fun providesOkHttpClient(
+        httpLogger: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(httpLogger)
+            .callTimeout(HTTP_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun providesHttpLogger() : HttpLoggingInterceptor {
+        return run {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+    }
+
+}
--- modules/src/main/java/app/lounge/users/Google.kt
+++
@@ -1,8 +0,0 @@
-package app.lounge.users
-
-/**
- * Implement API related to Google login flow only.
- * 1. Login api for google users.
- * 2. Parsing google user data. For now use typealias object, later we will refactor with generic type
- * 3. Add unit test cases for the api functions.
- * */
