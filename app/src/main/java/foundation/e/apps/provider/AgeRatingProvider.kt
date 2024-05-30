b3598e1c202616d7bf5393f26917d59e2cf94af4
Sayantan Roychowdhury <sayantan.rc@e.email>
2024-05-31 00:03:20 +0530

    create getAgeRatings()

--- app/src/main/java/foundation/e/apps/provider/AgeRatingProvider.kt
+++ app/src/main/java/foundation/e/apps/provider/AgeRatingProvider.kt
@@ -10,13 +10,22 @@ import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
+import foundation.e.apps.data.application.ApplicationRepository
+import foundation.e.apps.data.enums.Origin
 import foundation.e.apps.data.login.AuthenticatorRepository
+import foundation.e.apps.data.playstore.PlayStoreRepository
+import foundation.e.apps.install.pkg.AppLoungePackageManager
 import foundation.e.apps.provider.ProviderConstants.Companion.AGE_RATING
 import foundation.e.apps.provider.ProviderConstants.Companion.AUTHORITY
 import foundation.e.apps.provider.ProviderConstants.Companion.LOGIN_TYPE
 import foundation.e.apps.provider.ProviderConstants.Companion.PACKAGE_NAME
 import foundation.e.apps.provider.ProviderConstants.Companion.PATH_AGE_RATINGS
 import foundation.e.apps.provider.ProviderConstants.Companion.PATH_LOGIN_TYPE
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class AgeRatingProvider : ContentProvider() {
 
@@ -24,9 +33,15 @@ class AgeRatingProvider : ContentProvider() {
     @InstallIn(SingletonComponent::class)
     interface ContentProviderEntryPoint {
         fun getAuthenticationRepository(): AuthenticatorRepository
+        fun getPlayStoreRepository(): PlayStoreRepository
+        fun getApplicationRepository(): ApplicationRepository
+        fun getPackageManager(): AppLoungePackageManager
     }
 
     private lateinit var authenticatorRepository: AuthenticatorRepository
+    private lateinit var playStoreRepository: PlayStoreRepository
+    private lateinit var applicationRepository: ApplicationRepository
+    private lateinit var appLoungePackageManager: AppLoungePackageManager
 
     private val CODE_LOGIN_TYPE = 1
     private val CODE_AGE_RATING = 2
@@ -48,6 +63,7 @@ class AgeRatingProvider : ContentProvider() {
         val code = uriMatcher.match(uri)
         return when (code) {
             CODE_LOGIN_TYPE -> getLoginType()
+            CODE_AGE_RATING -> getAgeRatings(uri)
             else -> null
         }
     }
@@ -58,8 +74,37 @@ class AgeRatingProvider : ContentProvider() {
         return cursor
     }
 
-    private fun getAgeRatings(): Cursor {
+    private fun getAgeRatings(uri: Uri): Cursor {
         val cursor = MatrixCursor(arrayOf(PACKAGE_NAME, AGE_RATING))
+        val packagesNames = appLoungePackageManager.getAllUserApps().map { it.packageName }
+        runBlocking {
+            withContext(IO) {
+                val contentRatingsDeferred = packagesNames.map { packagesName ->
+                    async {
+                        val authData = runCatching {
+                            authenticatorRepository.gplayAuth
+                        }.getOrNull() ?: return@async null
+                        val appDetails =
+                            applicationRepository.getApplicationDetails(
+                                "",
+                                packagesName,
+                                authData,
+                                Origin.GPLAY
+                            )
+                        if (appDetails.first.package_name.isBlank()) return@async null
+                        playStoreRepository.updateContentRatingWithId(
+                            packagesName,
+                            appDetails.first.contentRating
+                        ).apply {
+                        }
+                    }
+                }
+                val contentsRatings = contentRatingsDeferred.awaitAll()
+                packagesNames.forEachIndexed { index, packageName ->
+                    cursor.addRow(arrayOf(packageName, contentsRatings[index]?.title))
+                }
+            }
+        }
         return cursor
     }
 
@@ -69,6 +114,9 @@ class AgeRatingProvider : ContentProvider() {
             EntryPointAccessors.fromApplication(appContext, ContentProviderEntryPoint::class.java)
 
         authenticatorRepository = hiltEntryPoint.getAuthenticationRepository()
+        playStoreRepository = hiltEntryPoint.getPlayStoreRepository()
+        applicationRepository = hiltEntryPoint.getApplicationRepository()
+        appLoungePackageManager = hiltEntryPoint.getPackageManager()
 
         return true
     }
