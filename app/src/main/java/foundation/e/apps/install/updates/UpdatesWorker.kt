565cfd73d40279ac0e310faa2ffa0faaf860ba77
Sayantan Roychowdhury <sayantan.rc@e.email>
2024-07-15 22:07:27 +0530

    integrate system apps api into updater

--- app/src/main/java/foundation/e/apps/install/updates/UpdatesWorker.kt
+++ app/src/main/java/foundation/e/apps/install/updates/UpdatesWorker.kt
@@ -20,6 +20,7 @@ import foundation.e.apps.data.blockedApps.BlockedAppRepository
 import foundation.e.apps.data.enums.ResultStatus
 import foundation.e.apps.data.enums.User
 import foundation.e.apps.data.application.data.Application
+import foundation.e.apps.data.gitlab.SystemAppsUpdatesRepository
 import foundation.e.apps.data.login.AuthenticatorRepository
 import foundation.e.apps.data.preference.DataStoreManager
 import foundation.e.apps.data.updates.UpdatesManagerRepository
@@ -40,6 +41,7 @@ class UpdatesWorker @AssistedInject constructor(
     private val authenticatorRepository: AuthenticatorRepository,
     private val appInstallProcessor: AppInstallProcessor,
     private val blockedAppRepository: BlockedAppRepository,
+    private val systemAppsUpdatesRepository: SystemAppsUpdatesRepository,
 ) : CoroutineWorker(context, params) {
 
     companion object {
@@ -62,6 +64,7 @@ class UpdatesWorker @AssistedInject constructor(
             }
 
             refreshBlockedAppList()
+            refreshEligibleSystemApps()
             checkForUpdates()
             Result.success()
         } catch (e: Throwable) {
@@ -77,6 +80,12 @@ class UpdatesWorker @AssistedInject constructor(
     private suspend fun refreshBlockedAppList() {
         if (isAutoUpdate) {
             blockedAppRepository.fetchUpdateOfAppWarningList()
+        }
+    }
+
+    private suspend fun refreshEligibleSystemApps() {
+        if (systemAppsUpdatesRepository.getEligibleSystemApps().isEmpty()) {
+            systemAppsUpdatesRepository.fetchEligibleSystemApps()
         }
     }
 
