f19b3f1db21f070887ebd04de577ebe2be468511
Sayantan Roychowdhury <sayantan.rc@e.email>
2024-07-18 16:35:59 +0000

    feat: (Issue 8124) Integrate system update API to updater
    See merge request e/os/apps!477

--- app/src/main/java/foundation/e/apps/install/updates/UpdatesWorkManager.kt
+++ app/src/main/java/foundation/e/apps/install/updates/UpdatesWorkManager.kt
@@ -47,8 +47,11 @@ object UpdatesWorkManager {
         return OneTimeWorkRequest.Builder(UpdatesWorker::class.java).apply {
             setConstraints(buildWorkerConstraints())
             addTag(USER_TAG)
-        }.setInputData(Data.Builder().putBoolean(UpdatesWorker.IS_AUTO_UPDATE, false).build())
-            .build()
+        }.setInputData(
+            Data.Builder()
+                .putBoolean(UpdatesWorker.IS_AUTO_UPDATE, false)
+                .build()
+        ).build()
     }
 
     private fun buildWorkerConstraints() = Constraints.Builder().apply {
