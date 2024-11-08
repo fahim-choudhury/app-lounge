2c0fb94a465e42d90bc6cccd7f94050383fdaaa4
Jonathan Klee <jonathan.klee@e.email>
2024-11-08 15:47:19 +0100

    Load shimmer when home fragment resumes

--- app/src/main/java/foundation/e/apps/ui/home/HomeFragment.kt
+++ app/src/main/java/foundation/e/apps/ui/home/HomeFragment.kt
@@ -258,7 +258,7 @@ class HomeFragment : TimeoutFragment(R.layout.fragment_home), ApplicationInstall
 
     override fun onResume() {
         super.onResume()
-        binding.shimmerLayout.startShimmer()
+        showLoadingUI()
         appProgressViewModel.downloadProgress.observe(viewLifecycleOwner) {
             updateProgressOfDownloadingAppItemViews(homeParentRVAdapter, it)
         }
